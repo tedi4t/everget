4d006f2579ec89f19a803de1a08ab9f6ccff7daa
Uryn Dmytro <dmytro@digia.co>
2022-08-28 23:35:17 +0300

    popup on contact form submit

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,15 +1,30 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import swal from 'sweetalert';
 
 import { ContactPage, Navbar } from '../components';
 import { ContactInfo } from '../components/ContactPage/ContactPage';
 
 const Contact: NextPage = () => {
 	const onSubmit = async (contactInfo: ContactInfo) => {
-		return fetch('/api/mails/send', {
+		fetch('/api/mails/send', {
 			body: JSON.stringify(contactInfo),
 			method: 'POST',
-		});
+		})
+			.then(() => {
+				swal({
+					title: 'Thank you for contacting us!',
+					text: 'We will contact you shortly',
+					icon: 'success',
+				});
+			})
+			.catch(() => {
+				swal({
+					title: 'We are sorry but this function is not available for now',
+					text: 'Contact us via everget@everget.com, please',
+					icon: 'info',
+				});
+			});
 	};
 
 	return (
