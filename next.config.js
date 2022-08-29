27d52cde98741dee0db6e019a9fd72c320ba882d
Uryn Dmytro <dmytro@digia.co>
2022-08-30 01:46:38 +0300

    updated next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+	reactStrictMode: true,
+	swcMinify: true,
+	images: {
+		deviceSizes: [128, 150, 165, 216, 240, 384, 400, 768],
+		imageSizes: [4, 8, 12, 16, 24, 32, 64, 96],
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
