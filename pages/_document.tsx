2ee7b49a40c8ac10b0ed2cfc622aa7eeb44e98a6
Uryn Dmytro <dmytro@digia.co>
2022-08-30 02:16:32 +0300

    changed strategy to strategy = afterInteractive,

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -29,20 +29,21 @@ class MyDocument extends Document {
 						async
 						id={'GA-source'}
 						src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-						strategy='lazyOnload'
+						strategy='afterInteractive'
 					/>
 					<Script
 						id={'GA-tracker'}
 						dangerouslySetInnerHTML={{
 							__html: `
-					  window.dataLayer = window.dataLayer || [];
-					  function gtag(){dataLayer.push(arguments);}
-					  gtag('js', new Date());
-					  gtag('config', '${GA_TRACKING_ID}', {
-					    page_path: window.location.pathname,
-					  });
-					`,
+								window.dataLayer = window.dataLayer || [];
+								function gtag(){dataLayer.push(arguments);}
+								gtag('js', new Date());
+								gtag('config', '${GA_TRACKING_ID}', {
+									page_path: window.location.pathname,
+								});
+							`,
 						}}
+						strategy='afterInteractive'
 					/>
 				</body>
 			</Html>
