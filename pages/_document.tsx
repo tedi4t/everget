31a3151dacd77a08ac969c6721286dd4afa55e15
Uryn Dmytro <dmytro@digia.co>
2022-08-30 01:23:27 +0300

    commented GA

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -24,19 +24,19 @@ class MyDocument extends Document {
 					<Main />
 					<NextScript />
 
-					<script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
-					<script
-						dangerouslySetInnerHTML={{
-							__html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-						}}
-					/>
+					{/*<script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />*/}
+					{/*<script*/}
+					{/*	dangerouslySetInnerHTML={{*/}
+					{/*		__html: `*/}
+					{/*  window.dataLayer = window.dataLayer || [];*/}
+					{/*  function gtag(){dataLayer.push(arguments);}*/}
+					{/*  gtag('js', new Date());*/}
+					{/*  gtag('config', '${GA_TRACKING_ID}', {*/}
+					{/*    page_path: window.location.pathname,*/}
+					{/*  });*/}
+					{/*`,*/}
+					{/*	}}*/}
+					{/*/>*/}
 				</body>
 			</Html>
 		);
